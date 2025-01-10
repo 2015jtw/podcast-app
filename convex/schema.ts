2fe3d5377a8ebdb0e515fa6aca2c25dbcc99d889
Josh <2015jtw@gmail.com>
2025-01-10 12:08:49 +0100

    trying to fix clerk webhook issue

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,15 +1,15 @@
-import { defineTable, defineSchema } from "convex/server";
+import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
   podcasts: defineTable({
-    audioStorageId: v.optional(v.id("_storage")),
-    imageStorageId: v.optional(v.id("_storage")),
     user: v.id("users"),
     podcastTitle: v.string(),
     podcastDescription: v.string(),
     audioUrl: v.optional(v.string()),
+    audioStorageId: v.optional(v.id("_storage")),
     imageUrl: v.optional(v.string()),
+    imageStorageId: v.optional(v.id("_storage")),
     author: v.string(),
     authorId: v.string(),
     authorImageUrl: v.string(),
